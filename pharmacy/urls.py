099df5683da78ddc42721b5d90c4fa2b1f428a3a
Omnia Gamal <omniagamal5588@gmail.com>
2023-05-26 16:39:12 +0300

    first commit

---
+++ pharmacy/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path,include
+from rest_framework_simplejwt import views as jwt_views
+from . import views
+from pharmacy.views import PharmacyRegistrationView,PharmacyLoginView,LogOutView,RestPasswordView,PharmacyProfileView,MedicineDetailes,MedicineInfo,OffersDetailes
+
+urlpatterns=[
+
+  path('pharmacyRegister/',PharmacyRegistrationView.as_view(),name='PharmacyRegister'),
+  path('pharmacyLogin/',PharmacyLoginView.as_view(),name='login'),
+  path('pharmacyProfile/', PharmacyProfileView.as_view(), name='profile'),
+  path('resetPassword/', RestPasswordView.as_view(), name='resetPassword'),
+  path('medicine/',MedicineDetailes.as_view(),name='medicine'),
+  path('med/<int:id>/',MedicineInfo.as_view()),
+  path('offers/',OffersDetailes.as_view(),name='offers'),
+  #re_path(r'^$', views.home, name='index'),
+  path('subscriptionToken/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+  path('subscriptionToken/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+  path("submit_subscription",views.submit_subscription),
+  path('logout/',LogOutView.as_view(),name='logout'),
+  
+  ]
